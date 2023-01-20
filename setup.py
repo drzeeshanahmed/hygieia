9f346b5ed951315bdbaa5e88b44873520dbacd38
Ahmed Lab <zeeshan.ahmed@hotmail.de>
2023-01-20 16:09:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hygieia',
-    version='1.0.0',
+    version='1.0.1',
     packages=['hygieia'],
     license='GNU General Public License v3.0',
     description='Hygieia utilizes feature selection to predict the clinical and genomic basis of disease.',
